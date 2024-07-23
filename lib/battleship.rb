a54244943969c4b6775aad7f357721963173da31
Renee Messersmith <morganrmessersmith@gmail.com>
2024-07-23 19:11:08 -0400

    feat: updated the battleship class

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -20,49 +20,50 @@ class Battleship
     def start_screen
         puts "Welcome to BATTLESHIP"
         puts "Enter p to play. Enter q to quit."
-        start_input = gets.chomp
-        if start_input.include("p")
-            "Thanks for playing!"
-        end
     end
 
     # run the game
     # game will require a loop to go until either the computers or the players
     # ships have been sunk
     def start
-        # puts "Welcome to BATTLESHIP"
-        # puts "Enter p to play. Enter q to quit."
-        # user_input = gets.chomp
-        # if user_input.include?("p")
-        #     print board.render
-        #     play_game(user_input)
-        #     board.render(true)
-        # elsif user_input.include?("q")
-        #     puts "Type 'ruby runner.rb' to start."
-        # end
+        start_screen
+        start_input = gets.chomp
+
+        until start_input == "p" || start_input == "q"
+            puts "Invalid entry. Please enter 'p' to play or 'q' to quit."
+            start_input = gets.chomp.downcase
+        end
+
+        if start_input.include?("p")
+            play_game
+        elsif start_input = 'q'
+            puts "Thanks for playing!"
+            exit
+        end
     end
 
     # we need to place a loop in here so that the game restarts with a clean board
-    # def play_game
-    #     @cruiser = Ship.new("Cruiser", 3)
-    #     @submarine = Ship.new("Submarine", 2)
+    def play_game
+        # computer places ships
+        computer_ship_placement(@)
+        computer_ship_placement(@)
+        place_ships_message
+        player_ship_placement(ship)
+        player_ship_placement(ship)
+        game_loop
+    end
 
-    #     puts "-------------------------------------"
-    #     puts "I have laid out my ships on the grid."
-    #     puts "You now need to lay out your two ships."
-    #     puts "The Cruiser is three units long and the Submarine is two units long."
-    #     puts "Place the Cruiser first."
+    def place_ships_message
+        puts "-------------------------------------"
+        puts "I have laid out my ships on the grid."
+        puts "You now need to lay out your two ships."
+        puts "The Cruiser is three units long and the Submarine is two units long."
+        puts "Place the Cruiser first."
+    end
 
-    #     placed_cruiser = gets.chomp.upcase
-    #     # require 'pry'; binding.pry
-    #     @cruiser.valid_placement?.valid
-    #     placed_cruiser.place_ship(@cruiser)
-
-    #     puts "Place the Submarine next."
-
-    #     placed_submarine = gets.chomp.upcase
-    #     placed_submarine.place_ship(@submarine)
-    # end
+    def player_ship_placement(ship)
+        user_input = 
+    end
 end
 
 # require 'pry'; binding.pry
